5e7e20ec4ef7adda1654d3cda302c5b62e059854
satori-01 <132801948+satori-01@users.noreply.github.com>
2023-07-22 01:06:21 +0600

    Add files via upload

---
+++ dz2/script.js
@@ -0,0 +1,10 @@
+var cities = ['Bishkek', 'Los Angeles', 'Chicago', 'New York', 'Houston', 'Dallas', 'Philadelphia', 'Seattle'];
+
+for (var i = 0; i < cities.length; i++) {
+  if (cities[i][0] === 'k' || cities[i][0] === 'K') {
+    console.log(`${cities[i]}`);
+  }
+else{
+console.log(`${cities[i]}`)
+}
+}  
